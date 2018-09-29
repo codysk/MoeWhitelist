88687ba27c58b3e507b6cb7a50d8b3c08fb060b6
Skywind <me@iskywind.com>
2018-09-30 00:35:08 +0800

    check if user is whitelisted && load whitelist from file

--- src/main/java/moe/two/minecraft/sponge/whitelist/moewhitelist/Moewhitelist.java
+++ src/main/java/moe/two/minecraft/sponge/whitelist/moewhitelist/Moewhitelist.java
@@ -1,16 +1,21 @@
 package moe.two.minecraft.sponge.whitelist.moewhitelist;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
-import org.spongepowered.api.config.ConfigDir;
 import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.event.game.state.GameStartingServerEvent;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.text.serializer.TextSerializers;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
 
 @Plugin(
         id = "moewhitelist",
@@ -26,9 +31,22 @@ public class Moewhitelist {
     @Inject
     private Logger logger;
 
+    @Inject
+    Game game;
+
+    private static Set<String> whitelist;
+
     @Listener
-    public void onServerStart(GameStartedServerEvent event) {
-        logger.info("MoeWhitelist Loaded~");
+    public void onServerStart(GameStartingServerEvent event) throws Exception {
+        logger.info("MoeWhitelist loading...");
+        Path world_directory_path = game.getSavesDirectory();
+        Path whitelist_file_path = world_directory_path.resolve("whitelist.json");
+
+        whitelist = loadWhitelist(whitelist_file_path);
+
+        // TODO: register command for manager interface
+
+        logger.info("MoeWhitelist loaded");
     }
 
     @Listener
@@ -39,7 +57,6 @@ public class Moewhitelist {
             return;
         }
         String username = event.getProfile().getName().get();
-        //TODO: op always whitelisted
         if (!isWhitelisted(username)) {
             logger.info("User " + username + " not in Whitelist!");
             event.setMessage(TextSerializers.FORMATTING_CODE.deserialize("User " + username + " not in Whitelist!"));
@@ -48,7 +65,38 @@ public class Moewhitelist {
     }
 
     private Boolean isWhitelisted(String username) {
-        //TODO: Chick if player is whitelisted
-        return false;
+        return whitelist.contains(username);
+    }
+
+    private Set<String> loadWhitelist(Path whitelist_file_path) throws Exception {
+        Set<String> whitelist = new HashSet<>();
+
+        if (Files.exists(whitelist_file_path)){
+
+            try {
+                String json_str = new String(Files.readAllBytes(whitelist_file_path));
+                JsonArray json_arr = (new JsonParser()).parse(json_str).getAsJsonArray();
+
+                for (JsonElement name_element:
+                        json_arr) {
+                    String name_str = name_element.getAsString();
+                    whitelist.add(name_str);
+                }
+
+            } catch (Exception e){
+                e.printStackTrace();
+                logger.warn("Load Whitelist File Err! using as empty");
+            }
+
+        } else {
+            logger.warn("Whitelist File Not Found. Create empty one");
+            try {
+                Files.write(whitelist_file_path, "[]".getBytes());
+            } catch (Exception e){
+                logger.error("Create Whitelist File failed!");
+                throw e;
+            }
+        }
+        return whitelist;
     }
 }
